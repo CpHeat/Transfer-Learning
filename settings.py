7c01a939220649d6364057f815ece9a5b8969616
CpHeat <charles.petit.59@gmail.com>
2025-05-23 16:05:34 +0200

    ready for testing

---
+++ settings.py
@@ -0,0 +1,33 @@
+"""
+Parameters
+rgb: are images treated as rgb or grayscale
+strategy: strategy to use for the transfered model: can be fine_tuning, feature_extraction or partial_fine_tuning
+fixed-layers : only used with partial fine-tuning, wil define how many layers will stay untouched
+epochs: how many epochs to go through
+batch_size: how many img by batch size
+img_size: self explanatory
+"""
+params = {
+    "rgb": True,
+    "strategy": "feature_extraction",
+    "fixed-layers": 5,
+    "epochs": 5,
+    "batch_size": 200,
+    "img_size": 200
+}
+model_name = "VGG16"
+
+folders = {
+    "train": {
+        "input": "data/2/chest_xray/train",
+        "output": "data/processed/train"
+    },
+    "test": {
+        "input": "data/2/chest_xray/test",
+        "output": "data/processed/test"
+    },
+    "val": {
+        "input": "data/2/chest_xray/val",
+        "output": "data/processed/val"
+    }
+}
